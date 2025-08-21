01474afa2bfd4edaf484799cc1c5b95784889cdc
sabbirahmed06112009p-oss <sabbirahmed06112009p@gmail.com>
2025-08-21 09:09:47 +0600

    added new content

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const myButton = document.querySelector('button');
 // এরপর আমরা বাটনটাতে একটা "ক্লিক" ইভেন্ট যুক্ত করব।
 myButton.addEventListener('click', function() {
   // বাটনে ক্লিক করলে এই কোডটা চলবে
-  alert("তুমি আমাকে ক্লিক করেছো! বাহ!");
+  alert("WELCOME TO SABBIR AHMED's WEBSITE !");
 });
