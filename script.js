2add65ec2b77db9b48e7581be0ffc2dffdb8c9a6
sabbirahmed06112009p-oss <sabbirahmed06112009p@gmail.com>
2025-09-10 15:59:19 +0600

    <তোমার মেসেজ এখানে লিখো>

--- script.js
+++
@@ -1,8 +0,0 @@
-// প্রথমে আমরা HTML থেকে বাটনটাকে খুঁজে বের করব।
-const myButton = document.querySelector('button');
-
-// এরপর আমরা বাটনটাতে একটা "ক্লিক" ইভেন্ট যুক্ত করব।
-myButton.addEventListener('click', function() {
-  // বাটনে ক্লিক করলে এই কোডটা চলবে
-  alert("WELCOME TO SABBIR AHMED's WEBSITE !");
-});
